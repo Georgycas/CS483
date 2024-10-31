c73b024372067cf87f2fd4f7d0ec9f5698c22a84
Georgycas <32694172+Georgycas@users.noreply.github.com>
2024-10-31 14:48:39 -0700

    N

---
+++ Lecture/Week 5/Module5_hill_climbing.py
@@ -0,0 +1,48 @@
+
+import sys
+from collections import deque
+
+from utils4e import *
+from node import*
+
+
+# ______________________________________________________________________________
+# argmin and argmax
+
+
+identity = lambda x: x
+
+
+def argmin_random_tie(seq, key=identity):
+    """Return a minimum element of seq; break ties at random."""
+    return min(shuffled(seq), key=key)
+
+
+def argmax_random_tie(seq, key=identity):
+    """Return an element with highest fn(seq[i]) score; break ties at random."""
+    return max(shuffled(seq), key=key)
+
+
+def shuffled(iterable):
+    """Randomly shuffle a copy of iterable."""
+    items = list(iterable)
+    random.shuffle(items)
+    return items
+#-------------------------------------------------
+
+def hill_climbing(problem):
+    """
+    [Figure 4.2]
+    From the initial node, keep choosing the neighbor with highest value,
+    stopping when no neighbor is better.
+    """
+    current = Node(problem.initial)
+    while True:
+        neighbors = current.expand(problem)
+        if not neighbors:
+            break
+        neighbor = argmax_random_tie(neighbors, key=lambda node: problem.value(node.state))
+        if problem.value(neighbor.state) <= problem.value(current.state):
+            break
+        current = neighbor
+    return current.state
---
+++ Lecture/Week 5/Module5_simulated_annealing.py
@@ -0,0 +1,24 @@
+def exp_schedule(k=20, lam=0.005, limit=100):
+    """One possible schedule function for simulated annealing"""
+    return lambda t: (k * np.exp(-lam * t) if t < limit else 0)
+
+def probability(p):
+    """Return true with probability p."""
+    return p > random.uniform(0.0, 1.0)
+
+
+def simulated_annealing(problem, schedule=exp_schedule()):
+    """[Figure 4.5] CAUTION: This differs from the pseudocode as it
+    returns a state instead of a Node."""
+    current = Node(problem.initial)
+    for t in range(sys.maxsize):
+        T = schedule(t)
+        if T == 0:
+            return current.state
+        neighbors = current.expand(problem)
+        if not neighbors:
+            return current.state
+        next_choice = random.choice(neighbors)
+        delta_e = problem.value(next_choice.state) - problem.value(current.state)
+        if delta_e > 0 or probability(np.exp(delta_e / T)):
+            current = next_choice
